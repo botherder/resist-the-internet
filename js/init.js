4efb4d9a43e2257bd641749e7a644c7a8267a91d
Nex <nex@nex.sx>
2018-07-25 09:38:19 +0200

    Loading appropriate icon at init

--- js/init.js
+++ js/init.js
@@ -2,4 +2,10 @@
     if (typeof localStorage.isFree === "undefined") {
         localStorage.isFree = true;
     }
+
+    if (localStorage.isFree === "true") {
+        browser.browserAction.setIcon({path: browser.extension.getURL("img/resist.png")});
+    } else {
+        browser.browserAction.setIcon({path: browser.extension.getURL("img/surveillance.png")});
+    }
 })();
