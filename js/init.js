1fc50bebb1d738b6c15d16de4f949f320e6a3b22
Nex <nex@nex.sx>
2018-11-02 19:16:00 +0100

    Oops, fixed license

--- js/init.js
+++ js/init.js
@@ -2,18 +2,18 @@
 //
 // This file is part of Resist the Internet!
 //
-// PhishDetect is free software: you can redistribute it and/or modify
+// Resist the Internet is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// PhishDetect is distributed in the hope that it will be useful,
+// Resist the Internet is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with PhishDetect.  If not, see <https://www.gnu.org/licenses/>.
+// along with Resist the Internet.  If not, see <https://www.gnu.org/licenses/>.
 
 (function() {
     if (typeof localStorage.isFree === "undefined") {
